ce4341b0748e075eae0f83311a34df939ec0b9d1
tilhi3 <>
2020-12-17 18:22:00 +0200

    Added hermes support and removed hacky workarounds

--- MessageWebView.js
+++ MessageWebView.js
@@ -2,72 +2,58 @@ import React from 'react'
 import { View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
-//https://github.com/facebook/react-native/issues/10865#issuecomment-536757883
-// Apply fix for newer react-native-webview not supporting window.postMessage
-const webViewFix = function() {
-  window.postMessage = function(data) {
-    window.ReactNativeWebView.postMessage(data);
-  };
+const getCaptchaContent = (siteKey: string, action: string) => {
+  return `<!DOCTYPE html>
+    <html>
+      <head> 
+        <style>  .text-xs-center { text-align: center; } .g-recaptcha { display: inline-block; } </style>
+        <script src="https://www.google.com/recaptcha/api.js?render=${siteKey}"></script>
+        <script type="text/javascript">
+          grecaptcha.ready(function() {
+            //execute invisible recaptcha
+            window.grecaptcha.execute('${siteKey}', { action: '${action}' })
+            .then(function(args) {
+              window.ReactNativeWebView.postMessage(args);
+            });
+          }); 
+        </script>
+      </head>
+    </html>`;
 };
-const windowPostMessageFix = '(' + String(webViewFix) + ')();';
-
-// fix https://github.com/facebook/react-native/issues/10865
-const patchPostMessageJsCode = `(${String(function() {
-    var originalPostMessage = window.postMessage
-    var patchedPostMessage = function(message, targetOrigin, transfer) {
-        originalPostMessage(message, targetOrigin, transfer)
-    }
-    patchedPostMessage.toString = function() {
-        return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage')
-    }
-    window.postMessage = patchedPostMessage
-})})();`
-
 
 export default class MessageWebView extends React.Component {
-    constructor(props) {
-        super(props)
-        this.postMessage = this.postMessage.bind(this);
-        this.reloadCaptcha = this.reloadCaptcha.bind(this);
+  constructor(props) {
+    super(props);
+    this.reloadCaptcha = this.reloadCaptcha.bind(this);
+    this.state = {
+      reloadCount: 0,
+    };
+  }
 
-        this.state = {
-            reloadCount: 0,
-        }
-    }
-    postMessage(action) {
-        this.WebView.postMessage(JSON.stringify(action))
-    }
+  reloadCaptcha() {
+    this.setState({reloadCount: this.state.reloadCount + 1});
+  }
 
-    getWebViewHandle = () => {
-        return this.webview;
-    }
+  render() {
+    const { html, source, url, onMessage, ...props } = this.props
 
-    reloadCaptcha() {
-        console.log('update key..'+this.state.reloadCount);
-        this.setState({reloadCount: this.state.reloadCount + 1});
-    }
-
-    render() {
-        const { html, source, url, onMessage, ...props } = this.props
-
-        return (
-            <View style={props.containerStyle}>
-            <WebView
-                {...props}
-                key={`try-${this.state.reloadCount}`}
-                style={props.containerStyle}
-                javaScriptEnabled
-                automaticallyAdjustContentInsets
-                injectedJavaScript={[
-                    windowPostMessageFix,
-                    patchPostMessageJsCode
-                ].join('')}
-                source={source ? source : html ? { html } : url}
-                ref={x => {this.webview = x}}
-                onMessage={e => onMessage(e.nativeEvent.data)}
-
-            />
-            </View>
-        )
-    }
+    return (
+      <View style={props.containerStyle}>
+        <WebView
+          {...props}
+          key={`try-${this.state.reloadCount}`}
+          style={props.containerStyle}
+          javaScriptEnabled
+          originWhitelist={['*']}
+          automaticallyAdjustContentInsets
+          mixedContentMode={'always'}
+          source={{
+            html: getCaptchaContent(this.props.siteKey, this.props.action),
+            baseUrl: this.props.url
+          }}
+          onMessage={e => onMessage(e.nativeEvent.data)}
+        />
+      </View>
+    );
+  }
 }
